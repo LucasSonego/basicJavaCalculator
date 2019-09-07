de24245c7fbac4e6685d46625856ddb786584dc2
Lucas Sônego <sonego_lucas@outlook.com>
2019-09-07 15:26:08 -0300

    basic operations

--- src/app/App.java
+++ src/app/App.java
@@ -1,7 +1,28 @@
 package app;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        float x, y;
+        char operator;
+
+        Scanner scan = new Scanner(System.in);
+
+        x = scan.nextFloat();
+        operator = scan.next().charAt(0);
+        y = scan.nextFloat();
+        
+        scan.close();
+
+        if (operator == '+') {
+            System.out.println(String.format("%.2f", x + y));
+        }else if(operator == '-'){
+            System.out.println(String.format("%.2f", x - y));
+        }else if(operator == '*'){
+            System.out.println(String.format("%.2f", x * y));            
+        }else if(operator == '/'){
+            System.out.println(String.format("%.2f", x / y));            
+        }
     }
 }
